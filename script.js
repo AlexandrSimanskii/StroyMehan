e30196f490d5ece944d8767cb88d4744152805b9
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2023-11-09 22:12:02 +0200

    08.11

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 const qualityElements = [
   ...document.querySelectorAll(".achievement-items__title"),
 ];
+const achievementSection = document.querySelector(".achievement");
+const achievementList = document.querySelector(".achievement-list");
+let hasIncreasedQuality = false;
 
-qualityElements.forEach((item) => {
-  increaseQuality(item);
-});
+window.onload = () => {
+  checkAndIncreaseQuantity();
+};
+
+window.onscroll = () => {
+  checkAndIncreaseQuantity();
+};
+
+function checkAndIncreaseQuantity() {
+  let top = window.scrollY;
+  let topElem = achievementSection.offsetTop;
+
+  let startPoint =
+    achievementSection.offsetTop -
+    window.innerHeight +
+    achievementSection.offsetHeight / 2;
+
+  if (top >= startPoint && !hasIncreasedQuality) {
+    qualityElements.forEach((item) => {
+      increaseQuality(item);
+      animateAchievementSection();
+      hasIncreasedQuality = true;
+    });
+  } else if (top < 300 && hasIncreasedQuality) {
+    hasIncreasedQuality = false;
+  }
+}
 
 function increaseQuality(elem) {
-  let num = parseInt(elem.textContent, 10);
-  elem.textContent = 0;
+  let num = parseInt(elem.textContent);
+  let finish = parseInt(elem.dataset.quantity);
 
   function increase() {
-    if (parseInt(elem.textContent, 10) < num) {
-      elem.textContent = parseInt(elem.textContent, 10) + 1;
-      setTimeout(increase, 2000/num);
+    if (finish > parseInt(elem.textContent)) {
+      console.log(elem);
+      elem.textContent = parseInt(elem.textContent) + 1;
+      setTimeout(increase, 1800 / finish);
     }
   }
 
   increase();
+}
+
+function animateAchievementSection() {
+  achievementList.classList.add("achievement-list--animate");
 }
